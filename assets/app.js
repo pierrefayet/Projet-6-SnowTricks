70a00f84b50ce52b6ecad401da30327a7b779933
pierre fayet <piero69450@gmail.com>
2024-02-02 21:41:38 +0100

    Added handler be allowed to access a page update trick, added a new form comment and implement CRUD

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,7 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
+ * This file will be included onto the page via the import() Twig function,
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss'
